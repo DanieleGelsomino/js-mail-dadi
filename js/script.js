c7abe57a2191a50610363371110220b3cd05773d
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-03-09 15:38:13 +0100

    add first exercise

--- js/script.js
+++ js/script.js
@@ -11,5 +11,26 @@ console.log('JS OK!')
     Stabilire il vincitore, in base a chi fa il punteggio più alto.         
 */
 
+const buttonLogin = document.querySelector('.login > button');
+buttonLogin.addEventListener('click', function(){
 
+    const listEmail = ["danny.gelso@gmail.com", "daniele.gelsomino@gmail.com, daniele.gelso@icloud.com"];
 
+    const imputLogin = document.querySelector(".login .email");
+
+    const emailValue = imputLogin.value;
+
+    for (let i = 0; i < listEmail.length; i++) {
+
+        if (emailValue === listEmail[i]) {
+            alert("Accesso eseguito!");
+            break;
+            
+        } else {
+            alert("Accesso negato! questo indirizzo email non è associato ad alcun utente perciò non è valido. Inserisci un indirizzo email corretto");
+            break;
+            
+        }
+    }
+
+})
